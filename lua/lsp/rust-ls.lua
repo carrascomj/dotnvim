fdfe94b36038a11e5cdd824a271590f7890db4da
carrascomj <carrascomurielj@gmail.com>
2021-05-01 11:05:25 +0200

    refactor: rely on local installation for rustanalyzer instead of LspInstall

--- lua/lsp/rust-ls.lua
+++ lua/lsp/rust-ls.lua
@@ -1,4 +1,17 @@
-require'lspconfig'.rust_analyzer.setup{}
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- capabilities.textDocument.completion.completionItem.resolveSupport = {
+--   properties = {
+--     'documentation',
+--     'detail',
+--     'additionalTextEdits',
+--   }
+-- }
+-- 
+-- require'lspconfig'.rust_analyzer.setup {
+--   capabilities = capabilities,
+-- }
+require'lspconfig'.rust_analyzer.setup {}
 -- 	cmd = {DATA_PATH .. "/lspinstall/rust/rust_analyzer"},
 -- 	on_attach = require'lsp'.common_on_attach
 -- }
