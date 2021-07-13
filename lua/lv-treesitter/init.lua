f019f5ae0a70b2cbf39390767beca976220757b1
carrascomj <carrascomurielj@gmail.com>
2021-07-14 00:24:41 +0200

    fix: change treesitter stuff

--- lua/lv-treesitter/init.lua
+++ lua/lv-treesitter/init.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = O.treesitter.ensure_installed, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "all",
     ignore_install = O.treesitter.ignore_install,
     highlight = {
-        enable = O.treesitter.highlight.enabled -- false will disable the whole extension
+        enable = true -- false will disable the whole extension
     },
     -- indent = {enable = true, disable = {"python", "html", "javascript"}},
     -- TODO seems to be broken
-    indent = {enable = {"javascriptreact"}},
+    indent = {enable = {"python"}},
     autotag = {enable = true},
 }
 
