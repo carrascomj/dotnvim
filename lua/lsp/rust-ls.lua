ec3c40cf2b5f97d232a8fe4a8c2ee24a33fdef16
carrascomj <carrascomurielj@gmail.com>
2021-11-25 20:27:17 +0100

    enhance(lsp): tweaks on rust-analyzer features

--- lua/lsp/rust-ls.lua
+++ lua/lsp/rust-ls.lua
@@ -11,7 +11,28 @@
 -- require'lspconfig'.rust_analyzer.setup {
 --   capabilities = capabilities,
 -- }
-require'lspconfig'.rust_analyzer.setup {}
+require'lspconfig'.rust_analyzer.setup {
+	settings = {
+	["rust-analyzer"] = {
+      assist = {
+        importMergeBehavior = "last",
+        importPrefix = "by_self",
+      },
+      diagnostics = {
+        disabled = { "unresolved-import", "missing-unsafe" }
+      },
+      cargo = {
+          loadOutDirsFromCheck = true
+      },
+      procMacro = {
+          enable = true
+      },
+      checkOnSave = {
+          command = "clippy"
+      },
+  }
+		}
+}
 -- 	cmd = {DATA_PATH .. "/lspinstall/rust/rust_analyzer"},
 -- 	on_attach = require'lsp'.common_on_attach
 -- }
