9818a99a6a6b1080ee6072d1f5a6100863d4a635
carrascomj <carrascomurielj@gmail.com>
2021-12-03 16:41:14 +0100

    feat: csv

--- lua/settings.lua
+++ lua/settings.lua
@@ -33,6 +33,19 @@ vim.cmd([[
 	let test#python#pytest#executable = 'python -m pytest'
 ]])
 
+-- csv weird
+vim.cmd([[
+if exists("did_load_csvfiletype")
+  finish
+endif
+let did_load_csvfiletype=1
+
+augroup filetypedetect
+  au! BufRead,BufNewFile *.csv,*.dat	setfiletype csv
+augroup END
+]]
+)
+
 -- show a line delimiter
 vim.cmd('set colorcolumn=80')
 vim.cmd('highlight ColorColumn guibg=#313643')
