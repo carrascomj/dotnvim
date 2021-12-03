ae359d22cf18a7210880314b7074d1411d39d172
carrascomj <carrascomurielj@gmail.com>
2021-12-03 16:41:44 +0100

    feat: load faster for the impatient.nvim's

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,99 +30,105 @@ end
 
 vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
 
-return require("packer").startup(
-    function(use)
-        -- Color
-        use "junegunn/rainbow_parentheses.vim"
-        use "norcalli/nvim-colorizer.lua"
+return require("packer").startup{function(use)
+				-- Fater plugin loading
+				use 'lewis6991/impatient.nvim'
+
+				-- Color
+				use "junegunn/rainbow_parentheses.vim"
+				use "norcalli/nvim-colorizer.lua"
 				use {"cespare/vim-toml", ft="toml", branch="main"}
 
-        -- Packer can manage itself as an optional plugin
-        use "wbthomason/packer.nvim"
+				-- Packer can manage itself as an optional plugin
+				use "wbthomason/packer.nvim"
 
 				-- Lsp
-        use {"neovim/nvim-lspconfig", opt = true}
-        use {"kabouzeid/nvim-lspinstall", opt = true}
+				use {"neovim/nvim-lspconfig", opt = true}
+				use {"kabouzeid/nvim-lspinstall", opt = true}
 				-- out-of-spec LSP (mainly for prose lintere)
-        use {"jose-elias-alvarez/null-ls.nvim",
-					  ft={"markdown", "txt", "tex"},
-			      config=function ()
-							local null_ls = require("null-ls")
-							local sources = {
-									null_ls.builtins.diagnostics.vale.with({filetypes={"markdown", "txt", "tex"}}),
-							}
-							null_ls.setup {sources = sources}
-						end
-    		}
+				use {"jose-elias-alvarez/null-ls.nvim",
+				ft={"markdown", "txt", "tex"},
+				config=function ()
+				local null_ls = require("null-ls")
+				local sources = {
+				null_ls.builtins.diagnostics.vale.with({filetypes={"markdown", "txt", "tex"}}),
+				}
+				null_ls.setup {sources = sources}
+				end
+				}
 
 				-- unit tests
-		    use "vim-test/vim-test"
+				use "vim-test/vim-test"
 
-        -- Telescope
-        use {"nvim-lua/popup.nvim", opt = true}
-        use {"nvim-lua/plenary.nvim", opt = true}
-        use {"nvim-telescope/telescope.nvim", opt = true}
+				-- Telescope
+				use {"nvim-lua/popup.nvim", opt = true}
+				use {"nvim-lua/plenary.nvim", opt = true}
+				use {"nvim-telescope/telescope.nvim", opt = true}
 				use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
 				-- Harpoon strange choice of the year
 				use "ThePrimeagen/harpoon"
 
 				-- Pop-up for code actions
 				use {
-					'weilbith/nvim-code-action-menu',
-					cmd = 'CodeActionMenu',
+				'weilbith/nvim-code-action-menu',
+				cmd = 'CodeActionMenu',
 				}
 				use 'kosayoda/nvim-lightbulb'
 
-        -- Autocomplete
-        -- use {"hrsh7th/nvim-compe", opt = true}
-        -- use {"hrsh7th/vim-vsnip", opt = true}
-        -- use {"rafamadriz/friendly-snippets", opt = true}
+				-- Autocomplete
+				-- use {"hrsh7th/nvim-compe", opt = true}
+				-- use {"hrsh7th/vim-vsnip", opt = true}
+				-- use {"rafamadriz/friendly-snippets", opt = true}
 				use "onsails/lspkind-nvim"
-        use "hrsh7th/nvim-cmp"
+				use "hrsh7th/nvim-cmp"
 				use 'hrsh7th/cmp-vsnip'
 				use 'hrsh7th/vim-vsnip'
-        use "hrsh7th/cmp-buffer"
-        use "hrsh7th/cmp-path"
-        use "hrsh7th/cmp-nvim-lua"
-        use "hrsh7th/cmp-nvim-lsp"
+				use "hrsh7th/cmp-buffer"
+				use "hrsh7th/cmp-path"
+				use "hrsh7th/cmp-nvim-lua"
+				use "hrsh7th/cmp-nvim-lsp"
 				use "windwp/nvim-autopairs"
 
-        -- Teesitter
-        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+				-- Teesitter
+				use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
 				use "nvim-treesitter/nvim-treesitter-textobjects"
 				-- use "nvim-treesitter/playground"
 
 				-- colorscheme
-        use "joshdick/onedark.vim"
+				use "joshdick/onedark.vim"
 
-        -- use {'lukas-reineke/indent-blankline.nvim', opt=true, branch = 'lua'}
-        use {"lewis6991/gitsigns.nvim", opt = true}
-        use {"terrortylor/nvim-comment", opt = true}
-        -- use {"kevinhwang91/nvim-bqf", opt = true}
+				-- use {'lukas-reineke/indent-blankline.nvim', opt=true, branch = 'lua'}
+				use {"lewis6991/gitsigns.nvim", opt = true}
+				use {"terrortylor/nvim-comment", opt = true}
+				-- use {"kevinhwang91/nvim-bqf", opt = true}
 
-        -- Surround
-        use "tpope/vim-surround"
+				-- Surround
+				use "tpope/vim-surround"
 
-        -- Status Line and Bufferline
-        use "glepnir/galaxyline.nvim"
-        use "romgrk/barbar.nvim"
-        use {"kyazdani42/nvim-web-devicons", opt = true}
+				-- Status Line and Bufferline
+				use "glepnir/galaxyline.nvim"
+				use "romgrk/barbar.nvim"
+				use {"kyazdani42/nvim-web-devicons", opt = true}
 
-        require_plugin("nvim-lspconfig")
-        require_plugin("nvim-lspinstall")
-        require_plugin("popup.nvim")
-        require_plugin("plenary.nvim")
-        require_plugin("telescope.nvim")
-        require_plugin("nvim-dap")
-        -- require_plugin("nvim-compe")
-        -- require_plugin("vim-vsnip")
-        require_plugin("nvim-autopairs")
-        require_plugin("nvim-treesitter")
-        require_plugin("nvim-web-devicons")
-        require_plugin("gitsigns.nvim")
-        require_plugin("nvim-comment")
-        -- require_plugin("nvim-bqf")
-        require_plugin("galaxyline.nvim")
-        require_plugin("barbar.nvim")
-    end
-)
+				-- weird csv stuff
+				use "chrisbra/csv.vim"
+
+				require_plugin("nvim-lspconfig")
+				require_plugin("nvim-lspinstall")
+				require_plugin("popup.nvim")
+				require_plugin("plenary.nvim")
+				require_plugin("telescope.nvim")
+				require_plugin("nvim-dap")
+				-- require_plugin("nvim-compe")
+				-- require_plugin("vim-vsnip")
+				require_plugin("nvim-autopairs")
+				require_plugin("nvim-treesitter")
+				require_plugin("nvim-web-devicons")
+				require_plugin("gitsigns.nvim")
+				require_plugin("nvim-comment")
+				-- require_plugin("nvim-bqf")
+				require_plugin("galaxyline.nvim")
+				require_plugin("barbar.nvim")
+		end,
+		compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua',
+}
