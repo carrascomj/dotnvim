24f80309fde4ad2ff3354f16724a485b4cf3cbf5
carrascomj <carrascomurielj@gmail.com>
2022-07-05 10:05:00 +0200

    refactor: change keybindings of completion

--- lua/cmp-me/init.lua
+++ lua/cmp-me/init.lua
@@ -16,8 +16,8 @@ cmp.setup({
 		end,
 	},
 	mapping = {
-		["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
