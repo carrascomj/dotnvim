3056367e06c52fe6bfca51a61b137a674e4dd1ed
carrascomj <carrascomurielj@gmail.com>
2023-01-25 11:24:49 +0100

    refactor: replace packer with lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,95 +1,91 @@
--- Plugins with Packer
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+-- Lazy bootstrapping
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
-return require("packer").startup({
-	function(use)
+return require("lazy").setup({
 		-- Fater plugin loading
 		-- use("lewis6991/impatient.nvim")
 
 		-- Color
-		use("junegunn/rainbow_parentheses.vim")
-		use{ "uga-rosa/ccc.nvim", config = function ()
+		{"junegunn/rainbow_parentheses.vim"},
+		{ "uga-rosa/ccc.nvim", config = function ()
 			vim.opt.termguicolors = true
 			require"ccc".setup { highlighter = { auto_enable = true } }
-		end }
-		-- use("norcalli/nvim-colorizer.lua")
-
-		-- Packer can manage itself as an optional plugin
-		use("wbthomason/packer.nvim")
+		end, lazy=true},
+		{"wbthomason/packer.nvim"},
 
 		-- Lsp
-		use "neovim/nvim-lspconfig"
-		use "kabouzeid/nvim-lspinstall"
-		use 'nvim-lua/lsp_extensions.nvim'
+		{"neovim/nvim-lspconfig"},
+		{"kabouzeid/nvim-lspinstall"},
+		{'nvim-lua/lsp_extensions.nvim'},
 
 		-- Unit tests
-		use("vim-test/vim-test")
+		{"vim-test/vim-test"},
 
 		-- Telescope
-		use "nvim-lua/popup.nvim"
-		use "nvim-lua/plenary.nvim"
-		use "nvim-telescope/telescope.nvim"
-		use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+		{"nvim-lua/popup.nvim"},
+		{"nvim-lua/plenary.nvim"},
+		{"nvim-telescope/telescope.nvim"},
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		-- Harpoon strange choice of the year
-		use("ThePrimeagen/harpoon")
+		{"ThePrimeagen/harpoon"},
 
 		-- Pop-up for code actions
-		use({
+		{
 			"weilbith/nvim-code-action-menu",
 			cmd = "CodeActionMenu",
-		})
+		},
 
 		-- Autocomplete
-		use("onsails/lspkind-nvim")
-		use("hrsh7th/nvim-cmp")
-		use("hrsh7th/cmp-vsnip")
-		use("hrsh7th/vim-vsnip")
-		use("rafamadriz/friendly-snippets")
-		use("hrsh7th/cmp-buffer")
-		use("hrsh7th/cmp-path")
-		use("hrsh7th/cmp-nvim-lua")
-		use("hrsh7th/cmp-nvim-lsp")
-		use("windwp/nvim-autopairs")
+		{"onsails/lspkind-nvim"},
+		{"hrsh7th/nvim-cmp", lazy=true},
+		{"hrsh7th/cmp-vsnip", lazy=true},
+		{"hrsh7th/vim-vsnip", lazy=true},
+		{"rafamadriz/friendly-snippets"},
+		{"hrsh7th/cmp-buffer"},
+		{"hrsh7th/cmp-path"},
+		{"hrsh7th/cmp-nvim-lua"},
+		{"hrsh7th/cmp-nvim-lsp"},
+		{"windwp/nvim-autopairs"},
 
 		-- Treesitter
-		use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-		use("nvim-treesitter/nvim-treesitter-textobjects")
-		use "nvim-treesitter/nvim-treesitter-refactor"
-		use {"mizlan/iswap.nvim", config=function ()
+		{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", lazy=false},
+		{"nvim-treesitter/nvim-treesitter-textobjects", lazy=true},
+		{"nvim-treesitter/nvim-treesitter-refactor"},
+		{"mizlan/iswap.nvim", config=function ()
 			require("iswap").setup()
-		end}
-    use "talbergs/context.nvim"
-		-- use "nvim-treesitter/playground"
+		end},
+    {"talbergs/context.nvim", lazy=true},
 		-- stan syntax
-		use "maedoc/stan.vim"
+		{"maedoc/stan.vim"},
 
 		-- colorscheme
-		use "navarasu/onedark.nvim"
+		{"navarasu/onedark.nvim", lazy=false},
 
-		use { "lewis6991/gitsigns.nvim", tag ="v0.5" }
-		use({ "terrortylor/nvim-comment" })
+		{"lewis6991/gitsigns.nvim", version ="v0.5" },
+		{"terrortylor/nvim-comment" },
 
 		-- Surround
-		use("tpope/vim-surround")
+		{"tpope/vim-surround"},
 
 		-- Status Line
-    use "NTBBloodbath/galaxyline.nvim"
-		use "nvim-tree/nvim-web-devicons"
+    {"NTBBloodbath/galaxyline.nvim"},
+		{"nvim-tree/nvim-web-devicons"},
 
 		-- weird csv stuff
-		use("chrisbra/csv.vim")
-		use{"jose-elias-alvarez/null-ls.nvim", config=function ()
+		{"chrisbra/csv.vim"},
+		{"jose-elias-alvarez/null-ls.nvim", config=function ()
 			require("null-ls").setup({
 				debug=true,
 				sources = {
@@ -97,35 +93,30 @@ return require("packer").startup({
 				}
 			})
 			end
-		}
+		},
 
-		use {
+		{
 			'chipsenkbeil/distant.nvim',
 			branch = "v0.2",
-config = function()
-    require('distant').setup {
-      -- Applies Chip's personal settings to every machine you connect to
-      --
-      -- 1. Ensures that distant servers terminate with no connections
-      -- 2. Provides navigation bindings for remote directories
-      -- 3. Provides keybinding to jump into a remote file's parent directory
-			ssh = { ssh_identity_file = {'/home/georg/.ssh/qmcm01_key.pem' }},
-			["*"] = {dir = { mappings = { ['<Return>'] = require('distant.nav.actions').edit } }}
-    }
-  end
-		}
+			config = function()
+    		require('distant').setup {
+		      -- Applies Chip's personal settings to every machine you connect to
+		      --
+		      -- 1. Ensures that distant servers terminate with no connections
+		      -- 2. Provides navigation bindings for remote directories
+		      -- 3. Provides keybinding to jump into a remote file's parent directory
+					ssh = { ssh_identity_file = {'/home/georg/.ssh/qmcm01_key.pem' }},
+					["*"] = {dir = { mappings = { ['<Return>'] = require('distant.nav.actions').edit } }}
+    		}
+  		end
+		},
 
-		use({
+		{
 			"glepnir/lspsaga.nvim",
 			branch = "main",
 			config = function()
 					local saga = require("lspsaga")
-					saga.init_lsp_saga()
+					saga.setup()
 			end,
-		})
-  	if packer_bootstrap then
-    	require('packer').sync()
-  	end
-	end,
-	compile_path = vim.fn.stdpath("config") .. "/lua/packer_compiled.lua",
+		}
 })
